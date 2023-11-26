47edf8a0aa5b7edd3817e710cc525881c4b192f3
nhutlong-nguyen <nhutlongnguyen.work@gmail.com>
2023-11-26 17:56:12 +0200

    completed execise 2.11 (phonebook)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter  from './components/Filter';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]); 
+  const [persons, setPersons] = useState([]); 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
 
 
   const addPerson = (event) => {
     event.preventDefault();
 
     // Check if the newName already exists in the persons array
-    const nameExists = persons.some(person => person.name.toLowerCase() === newName.toLocaleLowerCase())
+    const nameExists = persons.some(person => person.name.toLowerCase() === newName.toLocaleLowerCase());
     
     if (nameExists) {
       alert(`${newName} is already added to the phonebook`);
@@ -72,7 +76,7 @@ const App = () => {
       <Persons filteredPersons={filteredPersons} /> 
     </div>
   );
-}
+};
 
 export default App;
 
