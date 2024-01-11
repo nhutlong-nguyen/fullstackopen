952353b96a7631ee4411801b451d8c7f976a35aa
nhutlong-nguyen <nhutlongnguyen.work@gmail.com>
2024-01-11 05:44:26 +0200

    separated test files

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -46,16 +46,16 @@ const mostLikes = (blogs) => {
   
     const likesByAuthor = _(blogs)
       .groupBy('author') // Group the blog posts by the 'author' property.
-      .tap((grouped) => console.log('After groupBy:', grouped)) // Log the grouped blogs.
+      .tap((grouped) => console.log('After groupBy:', grouped)) 
       .map((posts, author) => ({
         author: author,
         likes: _.sumBy(posts, 'likes') // Sum the 'likes' for each author's posts.
       }))
-      .tap((summed) => console.log('After map/sumBy:', summed)) // Log the summed likes.
+      .tap((summed) => console.log('After map/sumBy:', summed))
       .value() // Extract the value from the chain.
   
     const topAuthor = _.maxBy(likesByAuthor, 'likes')
-    console.log('Most liked author:', topAuthor) // Log the author with most likes.
+    console.log('Most liked author:', topAuthor) 
   
     return topAuthor
   }
